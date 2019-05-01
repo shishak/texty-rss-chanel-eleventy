8847a38115ffc567888c298a6a52ea63530446e6
Shishak <semenshishak@gmail.com>
2019-05-01 09:26:18 +0300

    Update medium.js

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'https://medium.com/feed/netlify';
+var url = process.env.MEDIUM_FEED ||'http://35.188.114.84/feed/';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
