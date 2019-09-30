bc69b8e7296de22b797c8eced8cf2e406d257d8a
Shishak <semenshishak@gmail.com>
2019-09-30 09:47:14 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,17 @@ module.exports = function(eleventyConfig) {
     },
     feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/netlify'
   };
- 
+  
+};
+
+
+module.exports = function(eleventyConfig) {
+  // Output directory: _site
+
+  // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("img");
   
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // If you use a subdirectory, it’ll copy using the same directory structure.
+  eleventyConfig.addPassthroughCopy("css/fonts");
 };
