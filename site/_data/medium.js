d373a1b3a2038df3c49fee7a3cb30f6b6e86b057
Shishak <semenshishak@gmail.com>
2019-09-30 04:14:44 +0300

    Update medium.js

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'http://54.93.211.230/rss/';
+var url = process.env.MEDIUM_FEED ||'http://3.120.187.86.xip.io/feed';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
