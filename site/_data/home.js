26ef0bdc07cf630880fc752c42c39f153dee6435
Shishak <semenshishak@gmail.com>
2019-10-14 16:02:36 +0300

    Update home.js

--- site/_data/home.js
+++ site/_data/home.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'http://www.pipes.digital/feed/l9vzKd91';
+var url = process.env.MEDIUM_FEED ||'http://www.pipes.digital/feed/AO0gyDNK';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
